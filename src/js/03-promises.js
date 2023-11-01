0e06fd43530551b8bf52bcf95edb12b67c4d5b9b
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-11-01 22:14:34 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,43 @@
 
 
+const setbackInput = document.querySelector('input[name="delay"]');
+const stepSetbackInput = document.querySelector('input[name="step"]');
+const amountInput = document.querySelector('input[name="amount"]');
+const btnSubmit = document.querySelector('button[type="submit"]');
+btnSubmit.addEventListener('click', start);
 
+let timer = null;
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    return Promise.resolve({ 'position': position, 'delay': delay });
+  } else {
+    return Promise.reject({ 'position': position, 'delay': delay });
+  }
+}
+
+function start(evt) {
+  evt.preventDefault();
+  const delay = delayInput.value;
+  const stepDelay = stepDelayInput.value;
+  let position = amountInput.value;
+  console.log({'position': position, 'delay': delay, 'stepDelay': stepDelay})
+  setTimeout(() => {
+    clearInterval(timerId);
+    timerId = setInterval(() => {
+
+      createPromise(position, stepDelay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      position--;
+      if (position === 0) { clearInterval(timerId); }
+    }, stepDelay)
+  }, delay);
+}
