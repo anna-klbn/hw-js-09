eae7ee9f9d007152a82742ee12dd78737c6891bd
anya-kashchenko <143807908+anna-klbn@users.noreply.github.com>
2023-11-03 21:18:13 +0200

    02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,8 +10,41 @@ const refs = {
 
 function convertMs(ms) {
     
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+
+  const days = pad(Math.floor(ms / day));
+  
+  const hours = pad(Math.floor((ms % day) / hour));
+ 
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+
 }
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: Date.now(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Please choose a date in the future');
+      selectedDates[0] = new Date();
+    } else {
+      refs.startBtn.disabled = false;
+      selectedTime = selectedDates[0];
+    }
+  },
+};
+
